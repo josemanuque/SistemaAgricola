f5ef43280ac61b7186cf45919bfff57b3cdf2ac1
josemanuque <josemanuque@gmail.com>
2022-08-27 00:42:48 -0600

    Implemented function to input login

--- console/proyecto1/proyecto1/main.c
+++ console/proyecto1/proyecto1/main.c
@@ -2,8 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+//Function that reads a string until enter is pressed and returns it
+char* readString() {
+    char* string = (char*)malloc(sizeof(char) * 100);
+    int i = 0;
+    char c;
+    while ((c = getchar()) != '\n' && string != NULL) {
+        string[i] = c;
+        i++;
+    }
+    if (string != NULL)
+        string[i] = '\0';
+    return string;
+}
+
+
+// Function that verifies login from strings
+void login(MYSQL* con) {
+    char* user;
+    char* password;
+
+    while (1) {
+        printf("Ingrese usuario: ");
+        user = readString();
+        printf("Ingrese contrasena: ");
+        password = readString();
+		
+        if (mysql_real_connect(con, "localhost", user, password,
+            NULL, 0, NULL, 0) == NULL) {
+            fprintf(stderr, "%s\n", mysql_error(con));
+            mysql_close(con);
+            con = mysql_init(NULL);
+            printf("\nUsuario o contrasena es incorrecto.\n");
+        }
+        else
+            break;
+    }
+    free(user);
+	free(password);
+}
+
+
 // Function that connects to MYSQL database and returns connection
+
 MYSQL* connectServer() {
+
     // Connect to MYSQL database
     MYSQL* con = mysql_init(NULL);
 
@@ -13,35 +59,16 @@ MYSQL* connectServer() {
         exit(1);
     }
 
-    if (mysql_real_connect(con, "localhost", "root", "Basad0.",
-        NULL, 0, NULL, 0) == NULL)
-    {
-        fprintf(stderr, "%s\n", mysql_error(con));
-        mysql_close(con);
-        exit(1);
-    }
+    login(con);
+
     return con;
 }
 
-//Function that reads a string until enter is pressed and returns it
-char* readString(){
-    char* string = (char*)malloc(sizeof(char)*100);
-    int i = 0;
-    char c;
-    while((c = getchar()) != '\n' && string != NULL){
-        string[i] = c;
-        i++;
-    }
-	if (string != NULL)
-        string[i] = '\0';
-    return string;
-}
 
 int main() {
-    printf("%s",readString());
     //menuPrincipal();
 	MYSQL* con = connectServer();
     mysql_query(con, "USE TEST"); 
-    //mysql_query(con, "CREATE TABLE prueba2 ( nombre VARCHAR(50) NOT NULL)");
+    mysql_query(con, "CREATE TABLE prueba2 ( nombre VARCHAR(50) NOT NULL)");
 }
 
